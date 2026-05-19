518f3c59f8053768ff0695042ddf34d27af54bff
Manuel Ponferrada <manuponf2007@gmail.com>
2026-05-19 08:49:57 +0200

    Comentario en PRIORIDAD que no tiene comentarios.

--- src/main/java/es/franciscodelosrios/plutonf/model/Prioridad.java
+++ src/main/java/es/franciscodelosrios/plutonf/model/Prioridad.java
@@ -1,5 +1,9 @@
 package es.franciscodelosrios.plutonf.model;
 
+/**
+ * Enum que da a elegir si la PRIORIDAD de una reparacion es
+ * BATA, ALTA O CRITICA
+ */
 public enum Prioridad {
     BAJA, ALTA, CRITICA
 }
